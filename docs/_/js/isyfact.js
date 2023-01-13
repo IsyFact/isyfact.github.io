927d008b0589a3058a2e76c93d7365699bbfcfd5
Jens Häckel (msg) <jens.haeckel@msg.group>
2023-01-13 16:21:09 +0000

    docs: provide isyfact documentation for commit 47658633

--- docs/_/js/isyfact.js
+++ docs/_/js/isyfact.js
@@ -1,3 +1,8 @@
 var links = document.querySelectorAll("a[href$='.xml'], a[href$='.json'], a[href$='.pdf']");
 for(var i= 0; i< links.length; i++)
     links[i].setAttribute('download','');
+
+const elements = document.getElementsByClassName("imageblock text-center");
+for (let element of elements) {
+  element.setAttribute("style", "text-align:center;");
+}
