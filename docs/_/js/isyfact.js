eb13dfeb525342724c7131861b8b41c0f821ca56
Jannik von Loga <jannik.vonloga@bva.bund.de>
2023-01-14 02:04:51 +0000

    docs: provide isyfact documentation for commit 029f164e

--- docs/_/js/isyfact.js
+++ docs/_/js/isyfact.js
@@ -1,8 +1,3 @@
 var links = document.querySelectorAll("a[href$='.xml'], a[href$='.json'], a[href$='.pdf']");
 for(var i= 0; i< links.length; i++)
     links[i].setAttribute('download','');
-
-const elements = document.getElementsByClassName("imageblock text-center");
-for (let element of elements) {
-  element.setAttribute("style", "text-align:center;");
-}
