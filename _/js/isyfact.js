84b55a4bf175269bf712ead9b5a00dedc68b586c
vonLogaJ <vonLogaJ@users.noreply.github.com>
2023-03-15 09:26:59 +0000

    Deploying to gh-pages from @ IsyFact/isyfact.github.io@b6d8b047fdc4afd9c3465942a5520398e5d74ff3 🚀

--- _/js/isyfact.js
+++ _/js/isyfact.js
@@ -1,3 +1,26 @@
 var links = document.querySelectorAll("a[href$='.xml'], a[href$='.json'], a[href$='.pdf']");
 for(var i= 0; i< links.length; i++)
     links[i].setAttribute('download','');
+
+
+/*
+ * Scrollbar for tables with width too large.
+ * Could happen if <code> is used in table cells.
+ * Overflow-Scrollbar can only be introduced to parent node of table node therefore script is needed.
+ * Only apply for tables created via markup (class: tableblock).
+ */
+{
+    var tableNodes = document.getElementsByTagName("table");
+    for (let tableNode of tableNodes) {
+        if (tableNode.classList.contains("tableblock")) {
+            const range = document.createRange();
+
+            const newParentForTable = document.createElement('div');
+            newParentForTable.classList.add("scrollbar-table");
+
+            range.selectNode(tableNode);
+            range.surroundContents(newParentForTable);
+        }
+    }
+}
+
